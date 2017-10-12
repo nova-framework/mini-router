368070aba36cc1b79ea7aaf64873344a95552570
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-12 23:42:12 +0300

    Implementation of System\Config\Config

---
+++ system/Config/Config.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace System\Config;
+
+
+class Config
+{
+    /**
+     * @var array
+     */
+    protected static $settings = array();
+
+
+    /**
+     * Get the registered settings.
+     * @return mixed|null
+     */
+    public static function all()
+    {
+        return static::$settings;
+    }
+
+    /**
+     * Return true if the key exists.
+     * @param string $key
+     * @return bool
+     */
+    public static function has($key)
+    {
+        return ! is_null(array_get(static::$settings, $key));
+    }
+
+    /**
+     * Get the value.
+     * @param string $key
+     * @return mixed|null
+     */
+    public static function get($key, $default = null)
+    {
+        return array_get(static::$settings, $key, $default);
+    }
+
+    /**
+     * Set the value.
+     * @param string $key
+     * @param mixed $value
+     */
+    public static function set($key, $value)
+    {
+        array_set(static::$settings, $key, $value);
+    }
+}
